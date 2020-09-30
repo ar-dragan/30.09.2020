b7140a08510e29895bf389f26929b92f6dab5c85
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-09-30 16:14:24 +0300

    Add files via upload

---
+++ prob1.py
@@ -0,0 +1,17 @@
+n=int(input("dati numarul culorii curcubeului"))
+if (n == 1):
+print("Culoarea rosie")
+if (n == 2):
+print("Culoarea oranj")
+if (n == 3):
+print("Culoarea galben")
+if (n == 4):
+print("Culoarea verde")
+if (n == 5):
+print("Culoarea albastru")
+if (n == 6):
+print(" Culoarea indigo")
+if (n == 7):
+print(" Culoarea violet")
+if((n<1) or (n>7)):
+print("Curcubeul are 7 culori")
