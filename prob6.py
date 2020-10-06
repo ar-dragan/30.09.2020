4c5735251062573b36391badc0c23b67cce9fa99
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-06 15:37:52 +0300

    Add files via upload

---
+++ prob6.py
@@ -0,0 +1,6 @@
+"""Doi copii au primit acelasi umar de mere.Introducand de la tastatura numarul de mere pimite ,afisati cate mere are fiecare copil,dupa ce primul copil maninca un marsi da unul celuilalt copil.Ex:Date de intrare:10,date de iesire :primul copil 8 mere,al doilea copil 11mere"""
+n=int(input("dati numarul de mere primit de copii:"))
+n1=n-2
+n2=n+1
+print("primul copil are:",n1,"mere")
+print("al doilea copil are",n2,"mere")
