c25e18dd234d3d603ab91ac435655f9376144f60
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-06 15:10:39 +0300

    Add files via upload

---
+++ prob5.py
@@ -0,0 +1,3 @@
+"""Ion si Vasile joaca urmatorul joc:ion spune un numar ,iar Vasile trebuie sa gaseasca cinci numere consecutive,crescatoare,numarul din mijloc fiind cel ales de ion.Ion spune 10, Vasile spune 8 9 10 11 12"""
+n=int(input("nr.spus de ion="))
+print(n-2,n-1,n+1,n+2,sep="")
