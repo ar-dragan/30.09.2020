7b2bbc56136bb50c0422a457c154c3de2c76756b
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-06 16:05:41 +0300

    Add files via upload

---
+++ prob7.py
@@ -0,0 +1,6 @@
+"""maria vrea sa verifice daca greutatea si inaltimea mei corespund varstei pe care o are.maria a gasit intr-o carte urmatoarele formule de calculmale greutatii si inaltimii unui copil,
+v,fiind varsta:greutatea=2*v+8(in kg), inaltimea=5*v+80(in cm).Realizati un program care sa citeasca varsta unui copil si sa afiseze greutatea si inaltimea ideala folosind aceste formule.""'
+v=int(input"dati varst:"))
+greutatea=2*v+8
+inaltimea=5*v+80       
+print("varianta ideala este :","greutatea","kg","iar","inaltimea",inaltimea,"centimetri")
