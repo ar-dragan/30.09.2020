9287e6b0be15accaa504017e59e6c9f9c83ece23
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-06 14:43:21 +0300

    Add files via upload

---
+++ prob3.py
@@ -0,0 +1,7 @@
+"""intrun autobus care pleaca in excursie sunt 7 copii
+De la inca 2 scoli urca alti copii, numarul acestora citinduse de la tastatura.Cati copii au plecat in excursie?
+Exemplu:Date de intrare: 15 20 Date de iesire: 42 copii """
+s1=int(input("dati numarul de copii care urca la statia 1:"))
+s2=int(input("dati numarul de copii care urca la statia 2:"))
+nt=s1+s2
+print("in excursie au plecat",nt,"copii")
