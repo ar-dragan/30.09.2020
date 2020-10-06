53d08619195499754bfa18a2f191e8921f8e200f
dragan arina <70951580+ar-dragan@users.noreply.github.com>
2020-10-06 16:47:37 +0300

    Add files via upload

---
+++ prob9.py
@@ -0,0 +1,5 @@
+"""Sunt date trei numere ,sa se calculeze toate sumele posibile de cate doua numere .Afisarea sa cuprinda si termenii sumei,nu numa valoarea ei.Exempli:Date de intrare 2 13 4  Date de iesire 13+2=15 2+4=6 13+4=17."""
+a=int(input("primul numar:"))
+b=int(input("al doilea numar:"))
+с=int(input("al treilea numar:"))
+print(a,"+",b,"=",a+b," ",a,"+",c,"=",a+c," ",b,"+",c,"=",b+c)
